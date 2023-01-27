35fe882e3a9b9096826b790232501e5cba0e04c7
Adam <ks20447@bristol.ac.uk>
2023-01-27 12:21:06 +0000

    Added part (c)

--- question3.py
+++ question3.py
@@ -10,11 +10,13 @@ import numpy as np
 
 
 def run():
-    a = np.array([5, 4, 9, 2, 0, 4, 7, 2])
+    a = np.array([5, 4, 9, 2, 0, 4, 7, 2 ,-9])
     print(f"Last entry: {a[-1]}")
     print(f"Entries 1 through 6: {a[1:6]}")
     print(f"All entries up to second to last: {a[:-2]}")
     print(f"Every other entry from first: {a[::2]}")
+    a[0:3] = 1
+    print(f"{a}")
 
 
 run()
