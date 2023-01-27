9bf16d4d64179a655cec74d74502052e55f697aa
Adam <ks20447@bristol.ac.uk>
2023-01-27 12:18:52 +0000

    Q3 commit

---
+++ question3.py
@@ -0,0 +1,20 @@
+"""_summary_
+Author: ks20447
+
+Created: 27/01/2023
+
+Python file for answering Week 13 Sci Comp python exercises
+"""
+
+import numpy as np
+
+
+def run():
+    a = np.array([5, 4, 9, 2, 0, 4, 7, 2])
+    print(f"Last entry: {a[-1]}")
+    print(f"Entries 1 through 6: {a[1:6]}")
+    print(f"All entries up to second to last: {a[:-2]}")
+    print(f"Every other entry from first: {a[::2]}")
+
+
+run()
